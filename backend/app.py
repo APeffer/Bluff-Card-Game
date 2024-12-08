27bb6d1746a383951adfd723b74cd9f2c47ed545
Muhammad-Qassim <Muhammadqassim92@gmail.com>
2024-12-08 00:22:07 -0600

    Added /start-game endpoint to initialize game state

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,22 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from bluff import BluffGame
 
 app = Flask(__name__)
+game_instance = None
 
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify({'message': 'Hello from Flask!'})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/start-game', methods=['POST'])
+def start_game():
+    global game_instance
+    num_players = request.json['num_players']
+    game_instance = BluffGame(num_players=num_players)
+    initial_state = {
+        "players" : []
+    }
+    for i in range(num_players):
+        player_info = {
+            "player_id": i,
+            "num_cards": len(game_instance.players[i].cards)
+        }
+        initial_state["players"].append(player_info)
+        
+    return jsonify(initial_state)
