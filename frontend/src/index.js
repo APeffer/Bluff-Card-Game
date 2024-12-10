c4a1e5af1f2b1774bd06dbd4a95fc63a06276c63
Muhammad-Qassim <Muhammadqassim92@gmail.com>
2024-12-09 22:02:18 -0600

    Refactor App to use React Router v6 and add GameBoard
    Updated React Router from Switch to Routes for v6 compatibility.
    
    Changed Route syntax to use element prop instead of component.
    
    Added a new route for the GameBoard component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./components/homepage/HomePage";
 import NavBar from "./components/navbar/NavBar";
@@ -8,28 +8,24 @@ import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
 import Scene from "./components/Scene";
 
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GameBoard from "./components/gameboard/Gameboard";
 
 const App = () => {
   return (
     <Router>
-      <div className="">
-      <NavBar />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/scene" component={Scene} />
-        </Switch>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/scene" element={<Scene />} />
+          <Route path="/game-board" element={<GameBoard />} />
+        </Routes>
       </div>
     </Router>
   );
 };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 ReactDOM.render(<App />, document.getElementById("root"));
